16027c39f9d107f353391daa315425bcb79da08d
Vinceno-Demo <155643471+Vinceno-Demo@users.noreply.github.com>
2024-01-04 21:44:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,8 @@ if uploaded_file is not None:
 
 latex_output = 'a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} = \sum_{k=0}^{n-1} ar^k = a \left(\frac{1-r^{n}}{1-r}\right)'
 
-st.latex(latex_output)
+st.latex(r'''
+    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+    \sum_{k=0}^{n-1} ar^k =
+    a \left(\frac{1-r^{n}}{1-r}\right)
+    ''')
