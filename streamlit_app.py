4dcff5b09e43e5ceab669688010b3bc6fd3e1efc
Vinceno-Demo <155643471+Vinceno-Demo@users.noreply.github.com>
2024-01-04 20:41:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+import cv2
 import streamlit as st
 from PIL import Image
 import numpy as np
@@ -24,8 +25,19 @@ if img_file_buffer is not None:
 
 
 
-uploaded_file = st.file_uploader("Upload Image")
-image = Image.open(uploaded_file)
-st.image(image, caption='Input', use_column_width=True)
-img = Image.open(image)
-img.show()
+# uploaded_file = st.file_uploader("Upload Image")
+# image = Image.open(uploaded_file)
+# st.image(image, caption='Input', use_column_width=True)
+# img = Image.open(image)
+# img.show()
+
+
+uploaded_file = st.file_uploader("Choose a image file", type="jpg")
+
+if uploaded_file is not None:
+    # Convert the file to an opencv image.
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    opencv_image = cv2.imdecode(file_bytes, 1)
+
+    # Now do something with the image! For example, let's display it:
+    st.image(opencv_image, channels="BGR")
