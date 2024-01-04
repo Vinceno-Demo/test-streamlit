e8b716059c0afde134af2fc8faef26dd3458ded2
Vinceno-Demo <155643471+Vinceno-Demo@users.noreply.github.com>
2024-01-04 21:34:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,3 @@
-import cv2
 import streamlit as st
 from PIL import Image
 import numpy as np
@@ -37,7 +36,5 @@ uploaded_file = st.file_uploader("Choose a image file", type="jpg")
 if uploaded_file is not None:
     # Convert the file to an opencv image.
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-    opencv_image = cv2.imdecode(file_bytes, 1)
-
-    # Now do something with the image! For example, let's display it:
-    st.image(opencv_image, channels="BGR")
+    image = Image.open(file_bytes)
+    st.write(image.shape)
